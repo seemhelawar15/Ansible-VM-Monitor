135a24978e907345e33561e10367f3f9e3512fe9
seemhelawar15 <143342940+seemhelawar15@users.noreply.github.com>
2025-10-24 01:30:59 +0530

    Update PEM file name in copy_pub_key.sh

---
+++ copy_pub_key.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define vars
+PEM_FILE="server.pem"
+PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
+USER="ubuntu"  # or ec2-user
+INVENTORY_FILE="inventory/aws_ec2.yaml"
+
+# Extract hostnames/IPs from dynamic inventory
+HOSTS=$(ansible-inventory -i $INVENTORY_FILE --list | jq -r '._meta.hostvars | keys[]')
+
+for HOST in $HOSTS; do
+  echo "Injecting key into $HOST"
+  ssh -o StrictHostKeyChecking=no -i $PEM_FILE $USER@$HOST "
+    mkdir -p ~/.ssh && \
+    echo \"$PUB_KEY\" >> ~/.ssh/authorized_keys && \
+    chmod 700 ~/.ssh && \
+    chmod 600 ~/.ssh/authorized_keys
+  "
+done
