3513bf367492b1017609df03f0d492b136fc2d7b
seemhelawar15 <143342940+seemhelawar15@users.noreply.github.com>
2025-10-24 01:28:29 +0530

    Add script to tag EC2 instances for monitor environment

---
+++ tag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Fetch instance IDs that match Environment=dev and Role=web
+instance_ids=$(aws ec2 describe-instances \
+  --filters "Name=tag:Environment,Values=dev" "Name=instance-state-name,Values=running" \
+  --query 'Reservations[*].Instances[*].InstanceId' \
+  --output text)
+
+# Sort instance IDs deterministically
+sorted_ids=($(echo "$instance_ids" | tr '\t' '\n' | sort))
+
+# Rename instances sequentially
+counter=1
+for id in "${sorted_ids[@]}"; do
+  name="web-$(printf "%02d" $counter)"
+  echo "Tagging $id as $name"
+  aws ec2 create-tags --resources "$id" \
+    --tags Key=Name,Value="$name"
+  ((counter++))
+done
